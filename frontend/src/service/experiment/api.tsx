c0cbdfcf146c1c0de989dc306c9025db190d8c57
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-10-20 15:18:21 +0900

    [feat]experiment service 만들기

--- frontend/src/service/experiment/api.tsx
+++ frontend/src/service/experiment/api.tsx
@@ -1,4 +1,5 @@
 import { AxiosInstance } from "axios";
+import { Experiment, ExperimentCreateDto } from "./interface";
 
 export class experimentService {
   private instance: AxiosInstance;
@@ -7,8 +8,50 @@ export class experimentService {
     this.instance = instance;
   }
 
-  // async getGroupById({ group_id }: { group_id: number }): Promise<Group> {
-  //   const res = await this.instance.get(`/group/${group_id}`);
-  //   return res.data;
-  // }
+  // Get all experiments by jobId
+  public async getAllExperimentsByJobId(jobId: number): Promise<Experiment[]> {
+    try {
+      const response = await this.instance.get<Experiment[]>(
+        `/experiment/job/${jobId}`
+      );
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Get experiment by ID
+  public async getExperimentById(experimentId: number): Promise<Experiment> {
+    try {
+      const response = await this.instance.get<Experiment>(
+        `/experiment/${experimentId}`
+      );
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Create a new experiment
+  public async createExperiment(
+    experimentData: ExperimentCreateDto
+  ): Promise<Experiment> {
+    try {
+      const response = await this.instance.post<Experiment>(
+        "/experiment",
+        experimentData
+      );
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async deleteExperiment(experimentId: number): Promise<void> {
+    try {
+      await this.instance.delete(`/experiment/${experimentId}`);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
